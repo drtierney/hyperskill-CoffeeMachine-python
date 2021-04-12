256e29acfbd79db6e1ffec9edac90fd8f677d319
drtierney <d.r.tierney@hotmail.co.uk>
2021-04-12 22:31:36 +0100

    Stage #3 completed
    Stage 3/6: Estimate the number of servings

--- Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/task/machine/coffee_machine.py
@@ -1,10 +1,18 @@
-WATER = 200
-MILK = 50
-BEANS = 15
+water_stock = int(input("Write how many ml of water the coffee machine has:\n"))
+milk_stock = int(input("Write how many ml of milk the coffee machine has:\n"))
+beans_stock = int(input("Write how many grams of coffee beans the coffee machine has:\n"))
 
-cups = int(input("Write how many cups of coffee you will need:\n"))
+required_cups = int(input("Write how many cups of coffee you will need:\n"))
 
-print(f'for {cups} cups you will need:')
-print(f'{cups * WATER} ml of water')
-print(f'{cups * MILK} ml of milk')
-print(f'{cups * BEANS} g of coffee beans')
+cups_per_water = water_stock // 200
+cups_per_milk = milk_stock // 50
+cups_per_beans = beans_stock // 15
+
+max_cups = min(cups_per_water, cups_per_milk, cups_per_beans)
+
+if required_cups < max_cups:
+    print(f"Yes, I can make that amount of coffee (and even {max_cups - required_cups} more than that)")
+elif required_cups > max_cups:
+    print(f"No, I can make only {max_cups} cups of coffee")
+else:
+    print("Yes, I can make that amount of coffee")
